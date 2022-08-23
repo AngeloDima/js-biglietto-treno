aab0e08f5dab39aa8863169448b237d2163211f5
Angelo <Angelodimauro91@gmail.com>
2022-08-23 14:29:14 +0200

    Prima fase

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+{
+const text = prompt('chilometri che vuole percorrere');
+const textAsNumber = parseInt(text);
+console.log('textAsNumber', textAsNumber);
+}
+
+
+const text = prompt('Età del passeggero');
+const textAsNumber = parseInt(text);
+console.log('textAsNumber', textAsNumber);
+
+
+
+
