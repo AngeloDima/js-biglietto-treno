0df8a6e2df1583f65bd124a787fa00b5a71ce4f5
Angelo <Angelodimauro91@gmail.com>
2022-08-23 14:50:33 +0200

    moltiplicazione chilometri

--- script.js
+++ script.js
@@ -2,18 +2,49 @@
 
 
 
-
+// Chilometri
 {
 const text = prompt('chilometri che vuole percorrere');
 const textAsNumber = parseInt(text);
 console.log('textAsNumber', textAsNumber);
+
+const chilometri =  0.31 - textAsNumber;
+
+
+
+
+const risposta = `chilometri da percorrere ${chilometri}`;
+
+if  ( isNaN(textAsNumber)) { 
+    // blocco di istruzioni 1
+    document.getElementById('Chilometri').innerHTML = 'non hai inserito un numero';
+  } else 
+      // blocco di istruzioni 2
+      document.getElementById('Chilometri').innerHTML= risposta;
 }
 
 
-const text = prompt('Età del passeggero');
+
+
+
+
+
+
+// Età
+{
+const text = prompt('Quando sei nato?');
 const textAsNumber = parseInt(text);
 console.log('textAsNumber', textAsNumber);
+const anni = 2022 - textAsNumber;
 
+const risposta = `la tua età è ${anni}`;
 
+if  ( isNaN(textAsNumber)) { 
+  // blocco di istruzioni 1
+  document.getElementById('Età').innerHTML = 'non hai inserito un numero';
+} else 
+    // blocco di istruzioni 2
+    document.getElementById('Età').innerHTML= risposta;
+}
 
 
