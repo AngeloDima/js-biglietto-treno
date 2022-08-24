58aee35833f74ef4b5b7dd97988c17eb7f1d6784
Angelo <Angelodimauro91@gmail.com>
2022-08-24 02:13:17 +0200

    sconti

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
 
 
 
-
-// Chilometri
-{
-const text = prompt('chilometri che vuole percorrere');
-const textAsNumber = parseInt(text);
-console.log('textAsNumber', textAsNumber);
-
-const chilometri =  0.31 - textAsNumber;
+ //Età Viaggio-----------------------------------------------------------
+ const Chilometri = prompt('Quanti chilometri devi percorrere?');
+ const Km = parseInt(Chilometri);
 
 
+ const AnnoNascita = prompt('Quando sei nato?');
+ const Anni = parseInt(AnnoNascita);
+ console.log('Anni', Anni);
+ const anni = 2022 - Anni;
+ const risposta2 = `la tua età è ${anni}`;
 
-
-const risposta = `chilometri da percorrere ${chilometri}`;
-
-if  ( isNaN(textAsNumber)) { 
+ if  ( isNaN(Anni)) { 
     // blocco di istruzioni 1
-    document.getElementById('Chilometri').innerHTML = 'non hai inserito un numero';
-  } else 
+    document.getElementById('Età').innerHTML = 'non hai inserito un numero';
+  } else { 
       // blocco di istruzioni 2
-      document.getElementById('Chilometri').innerHTML= risposta;
-}
+      document.getElementById('Età').innerHTML= risposta2;
+  }
+  
+
+ 
+ console.log('Km', Km);
+ const risposta = `Devi percorrere  ${Km}`;
+
+ const costo = Km * 0.21
+ const soldi = `Costo Biglietto  ${costo}`;
+
+
+//  Variabile Minorenne
+
+
+let Min = anni < 18;
+const Mino = costo * 0.8
+const Sconto1 = `Prezzo scontato per Minorenni  ${Mino}`;
+
+let Mag = anni > 65;
+const Maggi = costo * 0.6
+const Sconto2 = `Prezzo scontato per Maggiorenni  ${Maggi}`;
 
 
 
 
 
-
-
-
-// Età
-{
-const text = prompt('Quando sei nato?');
-const textAsNumber = parseInt(text);
-console.log('textAsNumber', textAsNumber);
-const anni = 2022 - textAsNumber;
-
-const risposta = `la tua età è ${anni}`;
-
-if  ( isNaN(textAsNumber)) { 
-  // blocco di istruzioni 1
-  document.getElementById('Età').innerHTML = 'non hai inserito un numero';
+ if  ( isNaN(Km)) { 
+   // blocco di istruzioni 1
+   document.getElementById('Chilometri').innerHTML = 'non hai inserito un numero';
 } else 
-    // blocco di istruzioni 2
-    document.getElementById('Età').innerHTML= risposta;
-}
+
+     // blocco di istruzioni 2
+     document.getElementById('Chilometri').innerHTML= risposta;
+     document.getElementById('Costo').innerHTML= soldi;
+     document.getElementById('Costo-Scontato1').innerHTML= Sconto1;
+     document.getElementById('Costo-Scontato2').innerHTML= Sconto2;
+
+
+     
+
+     
+
+
+
+
+
+
+
+
+
+// Età-----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+// Viaggio-----------------------------------------------------------------------
+
+
+    
+
+
+
+
 
 
